7b23130d898e43f0727901282b27f661c9c5965e
sumanmr9591 <suman.aqua.srk@gmail.com>
2023-09-21 12:25:55 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hi, I am from react"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
